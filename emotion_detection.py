32d41e5fd8609e56cad67cb938f2b831eaa3ed94
samanthabd <donaldson.sgb@gmail.com>
2025-03-26 17:18:49 -0400

    function set up

---
+++ emotion_detection.py
@@ -0,0 +1,18 @@
+import json
+import requests
+
+def emotion_detector(text_to_analyze):
+    ''' Send the emotion detection request and return the results
+    '''
+
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+
+    myobj = {"raw_document": {"text": text_to_analyze}}
+
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    response = requests.post(url, json=myobj, headers=header, timeout=10)
+
+    formatted_response = json.loads(response.text)
+
+    return formatted_response
