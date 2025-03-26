f3fee637c5bbd54913db2434b735c7588f47a561
samanthabd <donaldson.sgb@gmail.com>
2025-03-26 17:54:47 -0400

    format output

--- emotion_detection.py
+++ emotion_detection.py
@@ -5,7 +5,8 @@ def emotion_detector(text_to_analyze):
     ''' Send the emotion detection request and return the results
     '''
 
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/' /
+          'NlpService/EmotionPredict'
 
     myobj = {"raw_document": {"text": text_to_analyze}}
 
@@ -14,5 +15,12 @@ def emotion_detector(text_to_analyze):
     response = requests.post(url, json=myobj, headers=header, timeout=10)
 
     formatted_response = json.loads(response.text)
-
-    return formatted_response
+    emotion_scores = formatted_response["emotionPredictions"][0]['emotion']
+    return {
+        'anger': emotion_scores["anger"],
+        'disgust': emotion_scores["disgust"],
+        'fear': emotion_scores["fear"],
+        'joy': emotion_scores["joy"],
+        'sadness': emotion_scores["sadness"],
+        'dominant_emotion': max(emotion_scores, key=emotion_scores.get)
+    }
