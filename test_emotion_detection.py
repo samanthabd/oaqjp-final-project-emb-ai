f835fa6e9de35d02af31a107eedfb2d0ea9f4813
samanthabd <89150693+samanthabd@users.noreply.github.com>
2025-03-27 16:52:19 -0400

    Fix test class name

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -1,7 +1,7 @@
 from EmotionDetection.emotion_detection import emotion_detector
 import unittest
 
-class TestSentimentAnalyser(unittest.TestCase):
+class TestEmotionDetector(unittest.TestCase):
     def test_emotion_detector(self):
         result_1 = emotion_detector("I am glad this happened")
         self.assertEqual(result_1['dominant_emotion'], 'joy')
